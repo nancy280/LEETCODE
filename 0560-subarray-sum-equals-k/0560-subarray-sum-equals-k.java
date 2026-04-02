3fdbac8e9bcad34e464f7d444e18f325b66a9820
Nancy Saxena <129685311+nancy280@users.noreply.github.com>
2026-04-03 02:52:27 +0530

    Time: 23 ms (94%), Space: 49.7 MB (8.65%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,24 +1,40 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int sum=0;
-        int c=0;
-      for(int i=0;i<nums.length;i++)
-      {
-        sum+=nums[i];
-        nums[i]=sum;
-      }  
-      for(int i=0;i<nums.length;i++)
-      {
-        if(nums[i]==k)
-        c++;
-        for( int j=i+1;j<nums.length;j++)
-        {
-            if(nums[i]+k==nums[j])
-            {
-                c++;
-            }
+        int sum = 0;
+        int c = 0;
+
+        if(nums.length == 1 && nums[0] != k)
+            return 0;
+        else if(nums.length == 1 && nums[0] == k)
+            return 1;
+
+        ArrayList<Integer> hm = new ArrayList<>();
+
+        // prefix sum array (same as yours)
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            hm.add(sum);
         }
-      }
-      return c;
+
+        // NEW: frequency map
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            // case 1: subarray from index 0
+            if(hm.get(i) == k)
+                c++;
+
+            // case 2: check previous prefix sums
+            int need = hm.get(i) - k;
+            if(map.containsKey(need)) {
+                c += map.get(need);
+            }
+
+            // update map
+            map.put(hm.get(i), map.getOrDefault(hm.get(i), 0) + 1);
+        }
+
+        return c;
     }
 }
