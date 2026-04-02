b0430a2033dcef0fef4628c5612c0b3269624509
Nancy Saxena <129685311+nancy280@users.noreply.github.com>
2026-04-03 02:55:39 +0530

    Time: 4 ms (92.41%), Space: 83.1 MB (41.4%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,24 +1,13 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-
-        while (true) {
-            slow = nums[slow];
-            fast = nums[nums[fast]];// moving one more step than slow and imagien that all numbers are connected to each others as nodes of linkedlist
-
-            if (slow == fast) {
-                break;
-            }
+        for(int i=0;i<nums.length;i++)
+        {
+            int num=Math.abs(nums[i]);
+            if(nums[num-1]<0)
+            return num;
+            else
+            nums[num-1]*=-1;
         }
-
-        int slow2 = nums[0];
-
-        while (slow != slow2) {
-            slow = nums[slow];
-            slow2 = nums[slow2];
-        }
-
-        return slow;        
+        return -1;
     }
 }
